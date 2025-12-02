2c57d04e6e11f16005cffc0daaf6f021fc79197e
Emmanuel Ochieng <eochieng16@gmail.com>
2025-12-02 17:31:23 +0300

    setup 'QuickSand' custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from 'react';
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require('../assets/assets/fonts/Quicksand-Bold.ttf'),
+    "QuickSand-Medium": require('../assets/assets/fonts/Quicksand-Medium.ttf'),
+    "QuickSand-Regular": require('../assets/assets/fonts/Quicksand-Regular.ttf'),
+    "QuickSand-SemiBold": require('../assets/assets/fonts/Quicksand-SemiBold.ttf'),
+    "QuickSand-Light": require('../assets/assets/fonts/Quicksand-Light.ttf'),
+  })
+
+  useEffect(() => {
+    if (error) throw error
+    if (fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded, error])
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
