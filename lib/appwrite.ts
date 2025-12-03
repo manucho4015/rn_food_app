9f936bafaae407dec7e1b4287b575f1d340181c3
Emmanuel Ochieng <eochieng16@gmail.com>
2025-12-03 17:53:22 +0300

    appwrite config setup

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -3,5 +3,7 @@
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     prjectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-    platform: 'com.manucho.foodordering'
+    platform: 'com.manucho.foodordering',
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    userTableId: process.env.EXPO_PUBLIC_APPWRITE_TABLE_ID
 }
