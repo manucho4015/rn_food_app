215e597d0cdfecbcefc0d0ff72573996ec6de01b
Emmanuel Ochieng <eochieng16@gmail.com>
2025-12-03 13:37:38 +0300

    appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    prjectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: 'com.manucho.foodordering'
+}
