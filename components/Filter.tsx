c61c8762f4bff584eb89b04b1bc7d853d10e4468
Emmanuel Ochieng <eochieng16@gmail.com>
2025-12-09 14:56:03 +0300

    implement 'Filter' component

---
+++ components/Filter.tsx
@@ -0,0 +1,27 @@
+import { Category } from '@/type'
+import cn from 'clsx'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
+import { FlatList, Platform, Text, TouchableOpacity } from 'react-native'
+
+const Filter = ({ categories }: { categories: Category[] }) => {
+    const searchParam = useLocalSearchParams()
+    const [active, setActive] = useState(searchParam.category || '')
+
+    const handlePress = (id: string) => {
+        setActive(id)
+        if (id === 'All') router.setParams({ category: undefined })
+        else router.setParams({ category: id })
+    }
+
+    const filterData: (Category | { $id: string, name: string })[] = categories ? [{ $id: 'all', name: 'All' }, ...categories] : [{ $id: 'all', name: 'All' }]
+    return (
+        <FlatList data={filterData} renderItem={({ item }) => (
+            <TouchableOpacity key={item.$id} className={cn('filter', active === item.$id ? 'bg-amber-500' : 'bg-white')} style={Platform.OS === 'android' ? { elevation: 5, shadowColor: '#878787' } : {}} onPress={() => handlePress(item.$id)}>
+                <Text className={cn('body-medium', active === item.$id ? 'text-white' : 'text-gray-200')}>{item.name}</Text>
+            </TouchableOpacity>
+        )} keyExtractor={(item) => item.$id} horizontal showsHorizontalScrollIndicator={false} contentContainerClassName='gap-x-2 pb-3' />
+    )
+}
+
+export default Filter
